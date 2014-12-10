640621ae4ca6ed2f430c42a159668c3053779341
John Feminella <jxf@jxf.me>
2014-12-10 00:32:53 -0500

    Add mkcd function: mkdir && cd

--- home/.config/shell/aliases.sh
+++ home/.config/shell/aliases.sh
@@ -1,2 +1,12 @@
 # More verbose, colorful ls.
 alias ls='ls -l --all --group-directories-first --color=auto'
+
+function mkcd {
+  if [ ! -n "$1" ]; then
+    echo "missing a directory name"
+  elif [ -d $1 ]; then
+    echo "\'$1' already exists"
+  else
+    mkdir $1 && cd $1
+  fi
+}
