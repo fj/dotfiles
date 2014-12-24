92e73d136dc5d9119052982d85524eafb2dec5c0
John Feminella <jxf@jxf.me>
2014-12-24 08:59:58 -0500

    Adds shell commands for working with Ruby projects

--- home/.config/shell/additions-ruby.sh
+++ home/.config/shell/additions-ruby.sh
@@ -1,5 +1,18 @@
 # Add chruby.
 source /usr/local/share/chruby/chruby.sh
 
+# Add gem_home.
+source /usr/local/share/gem_home/gem_home.sh
+
 # Bundler shortcut.
 alias be='bundle exec'
+
+function rb-go() {
+  chruby .
+  gem_home .
+}
+
+function rb-stop() {
+  gem_home -
+  chruby system
+}
