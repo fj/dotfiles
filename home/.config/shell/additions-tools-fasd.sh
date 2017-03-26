0f4cb5ab509489a7d678affc49488d83ddaee116
John Feminella <jxf@jxf.me>
2017-03-25 23:32:43 -0400

    Use explicit and inlined fasd initialization

--- home/.config/shell/additions-tools-fasd.sh
+++ home/.config/shell/additions-tools-fasd.sh
@@ -1,3 +1,9 @@
 # Add fasd (https://github.com/clvv/fasd) autocompletion.
 # Use the included 'z' alias to jump to different directories.
-eval "$(fasd --init auto)"
+
+fasd_cache="$HOME/.fasd-init-bash"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
