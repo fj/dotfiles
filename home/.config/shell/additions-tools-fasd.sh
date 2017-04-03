b90f7ca55301b7490737a72180a73291d05fca34
John Feminella <jxf@jxf.me>
2017-04-02 22:56:07 -0400

    fasd: Explicitly call fasd with $HOME/bin prefix

--- home/.config/shell/additions-tools-fasd.sh
+++ home/.config/shell/additions-tools-fasd.sh
@@ -1,9 +1,7 @@
-# Add fasd (https://github.com/clvv/fasd) autocompletion.
-# Use the included 'z' alias to jump to different directories.
-
+#! /usr/bin/env bash
 fasd_cache="$HOME/.fasd-init-bash"
 if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
+  $HOME/bin/fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
 fi
 source "$fasd_cache"
 unset fasd_cache
