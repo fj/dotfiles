7b4262eb73c401636bf184512b078d08f00fea87
John Feminella <jxf@jxf.me>
2026-06-10 06:14:03 -0400

    Fix dircolors path and use batcat alias in fish config
    Read dircolors from ~/.dircolors and alias cat to batcat, the binary
    name bat ships under on Debian/Ubuntu.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,6 +6,6 @@ starship init fish | source
 contains "$HOME/.cargo/bin" $fish_user_paths; or set -Ua fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 contains "$HOME/.local/bin" $fish_user_paths; or set -Ua fish_user_paths "$HOME/.local/bin" $fish_user_paths
 
-eval (dircolors $HOME/.config/dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
+eval (dircolors $HOME/.dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
 
-alias cat bat
+alias cat batcat
